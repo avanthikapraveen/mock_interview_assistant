62047cc621fe58cad067ac12a295d5105b24b9d7
avanthika <avanthikapraveen7@gmail.com>
2025-08-17 13:25:31 +0530

    Implemented resume text extraction - docx filetype

--- resume_parser.py
+++ resume_parser.py
@@ -1,5 +1,6 @@
 import pdfplumber
 import os
+import docx
 
 def extract_text_from_pdf(file_path):
     text = ""
@@ -17,8 +18,30 @@ def extract_text_from_pdf(file_path):
     except Exception as e:
         return f"Error extracting text from PDF: {e}"
     
+def extract_text_from_docx(file_path):
+    text = ""
+
+    if not os.path.isfile(file_path):
+        return "Error: The specified file does not exist."
+    
+    try:
+        doc = docx.Document(file_path)
+        for para in doc.paragraphs:
+            text += para.text + "\n"
+        return text
+    except Exception as e:
+        return f"Error extracting text from DOCX: {e}"
+    
+def extract_resume_text(file_path):
+    if file_path.lower().endswith(".pdf"):
+        return extract_text_from_pdf(file_path)
+    elif file_path.lower().endswith(".docx"):
+        return extract_text_from_docx(file_path)
+    else:
+        return "Error: Unsupported file format."
+
 if __name__ == "__main__":
-    resume_path = 'avanthika.pdf'
-    extracted_text = extract_text_from_pdf(resume_path)
+    resume_path = 'avanthika_resume.pdf'
+    extracted_text = extract_resume_text(resume_path)
     print(extracted_text)
 
